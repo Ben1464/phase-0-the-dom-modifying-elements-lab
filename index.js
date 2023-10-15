5afa267d3e7bc790114cca7fbf1f8621e2e40bef
Benson Mwangi <kimaniben@gmail.com>
2023-10-15 07:32:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+const newElement = document.createElement('h1');
+newElement.id = 'victory';
+document.body.appendChild(newElement);
+const newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = "YOUR-NAME is the champion";
